0230afe08f3ca0fc09da59023dd8e569950b639b
lindath0905 <linda.thurner@outlook.at>
2026-03-16 19:54:10 +0100

    added footer

--- src/App.js
+++ src/App.js
@@ -3,5 +3,23 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">JupitSky - Weather App</div>;
+  return (
+    <div className="App">
+      JupitSky - Weather App
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/lindath0905" target="_blank">
+          Linda Thurner
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/lindath0905/jupitsky-weather-app"
+          target="_blank"
+        >
+          open-sourced on Github
+        </a>
+        .
+      </footer>
+    </div>
+  );
 }
